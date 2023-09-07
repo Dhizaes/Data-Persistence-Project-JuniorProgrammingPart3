ea2d69323f03faf651363aedb4b077a08c8d8395
Dhizaes <dhizaes@protonmail.com>
2023-09-07 12:53:48 +0300

    Implement saving and loading best score and the owner of best score

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,16 +13,30 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
+    public TMPro.TMP_Text bestScoreText;
+    private string bestScorer = "Unknown";
+    private int bestScore = 0;
+
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
 
-    
+    [Serializable]
+    class BestScoreData
+    {
+        public int score;
+        public string player;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        LoadBestScore();
+
+        bestScoreText.text = "Best Score: \"" + bestScorer + "\", " + bestScore;
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -45,7 +60,7 @@ public class MainManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
+                float randomDirection = UnityEngine.Random.Range(-1.0f, 1.0f);
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
                 forceDir.Normalize();
 
@@ -70,7 +85,52 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        if (m_Points >= bestScore)
+        {
+            UpdateBestScore(NameCarrier.instance.GetPlayerName(), m_Points);
+        }
+
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    private void UpdateBestScore(string name, int score)
+    {
+        bestScorer = name;
+        bestScore = score;
+        bestScoreText.text = "Best Score: \"" + bestScorer + "\", " + bestScore;
+
+        SaveBestScore();
+    }
+
+    public void SaveBestScore()
+    {
+        if (NameCarrier.instance != null)
+        {
+            BestScoreData data = new BestScoreData();
+            data.player = NameCarrier.instance.GetPlayerName();
+            data.score = m_Points;
+
+            string json = JsonUtility.ToJson(data);
+            System.IO.File.WriteAllText(Application.persistentDataPath + "/score.json", json);
+        }
+        else
+        {
+            Debug.Log("Error while getting instance of NameCarrier!");
+        }
+    }
+
+    public void LoadBestScore()
+    {
+        string path = Application.persistentDataPath + "/score.json";
+
+        if(System.IO.File.Exists(path))
+        {
+            string json = System.IO.File.ReadAllText(path);
+            BestScoreData data = JsonUtility.FromJson<BestScoreData>(json);
+
+            bestScore = data.score;
+            bestScorer = data.player;
+        }
     }
 }
